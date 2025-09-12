cb6f6834c6b790bec2d2f3df7d2bbb8224128ab5
Nick Edwards <nickcoed@gmail.com>
2025-09-12 15:51:58 -0600

    First commit for abram mod

---
+++ src/main/java/net/nick/abrammod/AbramMod.java
@@ -0,0 +1,16 @@
+package net.nick.abrammod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AbramMod implements ModInitializer {
+	public static final String MOD_ID = "abrammod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/nick/abrammod/AbramModClient.java
@@ -0,0 +1,10 @@
+package net.nick.abrammod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbramModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
